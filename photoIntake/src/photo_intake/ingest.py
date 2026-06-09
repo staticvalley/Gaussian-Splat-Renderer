ce4991e985312c54bb18e5d72ce3eb2f03a5ea4e
Nicholls164 <nicholls164@gmail.com>
2026-06-09 10:53:14 -0400

    [feat](project-struct): Initial python training pipeline

---
+++ photoIntake/src/photo_intake/ingest.py
@@ -0,0 +1,2 @@
+def hello():
+    print("Photo intake package working!")
--- photoIntake/test_gpu.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-x = torch.randn(5000, 5000, device="cuda")
-y = x @ x
-print(y.shape)
-print(torch.cuda.get_device_name(0))
