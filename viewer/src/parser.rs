1409ee5631436a328eb2ba6ff2cb617dc080d4b6
Jonathan Kočevar <jonathan.kocevar@gmail.com>
2026-06-17 05:32:15 -0400

    [fix](file-parser): simplified .ply file reading
    - switched to bytemuck byte casting from single float conversion
    - removed hacky lambdas, now just copying float slices

--- viewer/src/parser.rs
+++ viewer/src/parser.rs
@@ -15,8 +15,9 @@ pub struct GaussianVertex {
     opacity: f32,
     /// quaternion rotation
     rotation: [f32; 4],
-    /// high-order coefficients
-    spherical_harmonics: [f32; 45]
+    // high-order coefficients
+    // NOTE: for now, im commenting this out, because its a lot of data to add to each splat
+    // spherical_harmonics: [f32; 45]
 }
 
 pub fn parse_ply_file(path: &str) -> std::io::Result<Vec<GaussianVertex>> {
@@ -29,7 +30,7 @@ pub fn parse_ply_file(path: &str) -> std::io::Result<Vec<GaussianVertex>> {
 
     let mut vertex_count: usize = 0;
     let mut properties = Vec::new();
-    let mut has_spherical_harmonic_coefficient = false;
+    //let mut has_spherical_harmonic_coefficient = false;
 
     loop {
         let mut line = String::new();
@@ -38,9 +39,9 @@ pub fn parse_ply_file(path: &str) -> std::io::Result<Vec<GaussianVertex>> {
         let line = line.trim();
 
         // spherical harmonics are included in file
-        if line.contains("f_rest_0") {
-            has_spherical_harmonic_coefficient = true;
-        }
+        // if line.contains("f_rest_0") {
+        //     has_spherical_harmonic_coefficient = true;
+        // }
 
         // get vertex count
         if line.starts_with("element vertex") {
@@ -62,59 +63,38 @@ pub fn parse_ply_file(path: &str) -> std::io::Result<Vec<GaussianVertex>> {
 
     let property_count = properties.len();
 
-    /*  TODO: can use bytemuck to convert all data straight to f32s immediately and index like that
-        would be quicker in the longrun
-    */ 
-
     // read in byte data after header
     let data_size = vertex_count * property_count * std::mem::size_of::<f32>();
     let mut raw_bytes = vec![0u8; data_size];
     reader.read_exact(&mut raw_bytes)?;
+
+    // only working with floats, so cast the entire vector to floats
+    let float_data: &[f32] = bytemuck::cast_slice(&raw_bytes);
 
     let mut splats: Vec<GaussianVertex> = Vec::with_capacity(vertex_count);
     
     for v in 0..vertex_count {
         let base = v * property_count;
 
-        // read single f32, where i is the offset from the base of the current vertex
-        let read_f32 = |i: usize| {
-            let byte_offset = (base + i) * 4;
-            let b = &raw_bytes[byte_offset..byte_offset + 4];
-            f32::from_le_bytes(b.try_into().unwrap())
-        };
-
-        // reads n number of floats from raw file bytes, where n is the size of the array given
-        let read_property = |property_array: &mut[f32], start: usize| {
-            for i in 0..property_array.len() {
-                property_array[i] = read_f32(start + i);
-            };
-        };
-
-        let mut property_index = 0;
-
         let mut position = [0.0f32; 3];
-        read_property(&mut position, property_index);
-        property_index += 3;
+        position.copy_from_slice(&float_data[base..base + 3]);
 
         let mut scale = [0.0f32; 3];
-        read_property(&mut scale, property_index);
-        property_index += 3;
+        scale.copy_from_slice(&float_data[base + 3..base + 6]);
 
         let mut diffuse = [0.0f32; 3];
-        read_property(&mut diffuse, property_index);
-        property_index += 3;
+        diffuse.copy_from_slice(&float_data[base + 6..base + 9]);
 
-        let opacity = read_f32(property_index);
-        property_index += 1;
+        let opacity = float_data[base + 9];
 
         let mut rotation = [0.0f32; 4];
-        read_property(&mut rotation, property_index);
-        property_index += 4;
+        rotation.copy_from_slice(&float_data[base + 10..base + 14]);
+        // property_index += 4;
 
-        let mut sh = [0.0f32; 45];
-        if has_spherical_harmonic_coefficient {
-            read_property(&mut sh, property_index)
-        }
+        // let mut sh = [0.0f32; 45];
+        // if has_spherical_harmonic_coefficient {
+        //      sh.copy_from_slice(&float_data[base + 14.. base + 45]);
+        // }
 
         splats.push(GaussianVertex {
             position: position,
@@ -122,7 +102,7 @@ pub fn parse_ply_file(path: &str) -> std::io::Result<Vec<GaussianVertex>> {
             diffuse: diffuse,
             opacity: opacity,
             rotation: rotation,
-            spherical_harmonics: sh,
+            // spherical_harmonics: sh,
         });
     }
 
